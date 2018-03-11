86f23d9e5613b15fc28fd27e3daf2b3692c69aee
mmaliki <mm@inthekloud.com>
2018-03-12 01:47:01 +0200

    Adds web client to generate qr code

--- client/vcard_submit.php
+++ client/vcard_submit.php
@@ -1,5 +1,29 @@
 <?php
-echo "Submitted";
+
+require_once  "../php/qrcode.php";
+
+$name =  $_POST["full-name"];
+$organization = $_POST["org-name"];
+$title = $_POST["title"];
+$email = $_POST["email"];
+$workTel = $_POST["tel-work"];
+$mobile = $_POST["tel-mobile"];
+$address = $_POST["addr-work"];
+
+$vcardString = "BEGIN:VCARD\n";
+$vcardString .= "VERSION:4.0\n";
+
+$vcardString .= $name."\n";
+$vcardString .= $organization."\n";
+$vcardString .= $title."\n";
+$vcardString .= $workTel."\n";
+$vcardString .= $mobile . "\n";
+$vcardString .= $address ."\n";
+$vcardString .= $email ."\n";
+$vcardString .= "END";
+
+echo $vcardString;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +34,6 @@ echo "Submitted";
 
     <title>VCARD to QR Code</title>
     <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/floating-labels.css">
    
 </head>
 
