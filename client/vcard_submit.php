a492b373316b559cea7f92d0c24a2c5f3590fb4f
Beltone Support <beltonesupport@inthekloud.com>
2018-03-11 22:33:47 +0200

    Client web form to submit QR code

---
+++ client/vcard_submit.php
@@ -0,0 +1,23 @@
+<?php
+echo "Submitted";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>VCARD to QR Code</title>
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/floating-labels.css">
+   
+</head>
+
+<body>
+    <div class="container">
+       Results to go here
+    </div>
+</body>
+
+</html>
